fc87be5ee77afe21141c0fabd5eb9c4bc128c7ba
hduy2001 <nguyenhoangduy15102001@gmail.com>
2022-12-19 13:53:50 +0700

    done

---
+++ app/src/main/java/com/ddq/braintrain/models/FindWordGameModel.java
@@ -0,0 +1,37 @@
+package com.ddq.braintrain.models;
+
+public class FindWordGameModel {
+    private int id;
+    private String word;
+    private int complete_status;
+
+    public FindWordGameModel(int id, String word, int complete_status) {
+        this.id = id;
+        this.word = word;
+        this.complete_status = complete_status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public int getComplete_status() {
+        return complete_status;
+    }
+
+    public void setComplete_status(int complete_status) {
+        this.complete_status = complete_status;
+    }
+}
